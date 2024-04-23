fc57537b1ee61bd1904f55bdcf0866e6be5fdf1b
javiermartinezusm <javier.martinezf@usm.cl>
2024-04-22 20:25:30 -0400

    feat: manejo INTERNAL SERVER ERROR

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,58 +17,87 @@ const promotion_rules = [
 
 router.get('/health', getHealth)
 router.post('/api/get-promotion',(ctx, next) => {
-    // handle your post request here
-    ctx.body = ctx.request.body;
-    let aux = {
-        'status' : 'OK',
-        'cart_id' : ctx.body['cart_id'],
-        'details' : []
-    }
-    let input_error=0
-    for (let i = 0; i < ctx.body['items'].length; i++) {
-        let item = ctx.body['items'][i];
-        let total_price =0;
-        let promotion_applied = 'false'
-        if(item['promotion']=="Nx$"){
-            let quotient = Math.floor(item['amount']/promotion_rules[0]['n'])
-            let remainder = item['amount']%promotion_rules[0]['n']
-            if(quotient>0){
-                total_price = item['unit_base_price']*(quotient*promotion_rules[0]['n']*(1-promotion_rules[0]['discount_percentage']/100)+remainder)
+    try{
+        ctx.body = ctx.request.body;
+        let aux = {
+            'status' : 'OK',
+            'cart_id' : ctx.body['cart_id'],
+            'details' : []
+        }
+        let input_error=0
+        if(!ctx.body['cart_id']){
+            input_error=3
+        } else if(!ctx.body['items']){
+            input_error=3
+        }
+        for (let i = 0; i < ctx.body['items'].length; i++) {
+            if(input_error==3){
+                break
+            }
+            let item = ctx.body['items'][i];
+            if(!item['item_id']){
+                input_error=3
+                break
+            } else if(!item['promotion']){
+                input_error=3
+                break
+            } else if(!item['amount']){
+                input_error=3
+                break
+            } else if(!item['unit_base_price']){
+                input_error=3
+                break
+            }
+            let total_price =0;
+            let promotion_applied = 'false'
+            
+            if(item['promotion']=="Nx$"){
+                let quotient = Math.floor(item['amount']/promotion_rules[0]['n'])
+                let remainder = item['amount']%promotion_rules[0]['n']
+                if(quotient>0){
+                    total_price = item['unit_base_price']*(quotient*promotion_rules[0]['n']*(1-promotion_rules[0]['discount_percentage']/100)+remainder)
+                    promotion_applied = 'true'
+                }
+                else{
+                    total_price = item['unit_base_price']*item['amount']
+                    promotion_applied = 'false'
+                }
+            }
+            else if(item['promotion']=="AyA"){
+                total_price=item['amount']*(1-promotion_rules[1]['discount_percentage']/100)*item['unit_base_price']
                 promotion_applied = 'true'
             }
-            else{
-                total_price = item['unit_base_price']*item['amount']
+            else if(item['promotion']==""){
+                total_price=item['amount']*item['unit_base_price']
                 promotion_applied = 'false'
             }
+            else{
+                input_error=1
+                break
+            }
+            if(total_price<=0){
+                input_error=2
+                break
+            }
+            aux['details'].push({'item_id': item['item_id'], 'amount' : item['amount'], 'total_price' : total_price, 'promotion_applied': promotion_applied})
         }
-        else if(item['promotion']=="AyA"){
-            total_price=item['amount']*(1-promotion_rules[1]['discount_percentage']/100)*item['unit_base_price']
-            promotion_applied = 'true'
+        if(input_error==0){
+            ctx.body=aux;
+            ctx.status=200;
+        } else if (input_error==1){
+            ctx.status=400
+            ctx.body={'status' : 'NOK', 'error_message': "RULE DOES NOT EXIST"}
+        } else if (input_error==2){
+            ctx.status=400
+            ctx.body={'status' : 'NOK', 'error_message': "AMOUNT OR PRICE SHOULD BE GREATER THAN ZERO"}
+        } else if (input_error==3){
+            ctx.status=500
+            ctx.body={'status' : 'NOK', 'error_message': "INTERNAL SERVER ERROR"}
         }
-        else if(item['promotion']==""){
-            total_price=item['amount']*item['unit_base_price']
-            promotion_applied = 'false'
-        }
-        else{
-            input_error=1
-            break
-        }
-        if(total_price<=0){
-            input_error=2
-            break
-        }
-        aux['details'].push({'item_id': item['item_id'], 'amount' : item['amount'], 'total_price' : total_price, 'promotion_applied': promotion_applied})
+        return ctx
+    } catch(error){
+        ctx.status=500
+        ctx.body={'status' : 'NOK', 'error_message': "INTERNAL SERVER ERROR"}
     }
-    if(input_error==0){
-        ctx.body=aux;
-        ctx.status=200;
-    } else if (input_error==1){
-        ctx.status=400
-        ctx.body={'status' : 'NOK', 'error_message': "RULE DOES NOT EXIST"}
-    } else if (input_error==2){
-        ctx.status=400
-        ctx.body={'status' : 'NOK', 'error_message': "AMOUNT OR PRICE SHOULD BE GREATER THAN ZERO"}
-    }
-    return ctx
   })
 export default router
